04b7b12093c5486bf77a70a30eb52ec5c894ed4c
Repooc <tjcooper1982@gmail.com>
2023-11-17 16:10:26 -0500

    Intial Release 1.0
    * add readme
    
    * add .pkgmeta
    
    * Add commits.yml workflow
    
    * Proof of Concept - Alpha
    
    * Added release.yml
    
    * try single toc for multiple wow versions
    
    * Update .pkgmeta for single toc
    
    * Update .pkgmeta
    Removed duplicate entry
    
    * Removed CHANGELOG.txt
    BW Packager is auto generating CHANGELOG.md
    
    * Fix Missing Party Frames Hook
    
    * Update Readme.md
    
    * Update toc for 1.0

---
+++ ElvUI_HealerOnlyPower/Core.lua
@@ -0,0 +1,132 @@
+local E, L, P, V, G = unpack(ElvUI)
+local EP = E.Libs.EP
+local UF = E.UnitFrames
+local AddOnName, Engine = ...
+
+local module = E:NewModule(AddOnName, 'AceHook-3.0', 'AceEvent-3.0')
+module.Version = GetAddOnMetadata(AddOnName, 'Version')
+module.Configs = {}
+
+function module:Print(...)
+	(E.db and _G[E.db.general.messageRedirect] or _G.DEFAULT_CHAT_FRAME):AddMessage(strjoin('', E.media.hexvaluecolor or '|cff00b3ff', 'Healer Only Power:|r ', ...))
+end
+
+local function GetOptions()
+	for _, func in pairs(module.Configs) do
+		func()
+	end
+end
+
+local function shouldDisplayBar(frame)
+	if not frame then return end
+
+	local unitframeType = frame.unitframeType
+	if not unitframeType then return end
+	local hopDB = E.db.hop
+	if not hopDB.unitframe[unitframeType].healerOnly then return end
+
+	local role = UnitGroupRolesAssigned(frame.unit)
+	if role == 'HEALER' then
+		return true
+	end
+
+	if role ~= 'TANK' then return end
+	local _, class = UnitClass(frame.unit)
+
+	local showTank = hopDB.unitframe[unitframeType]
+	if (class == 'DEATHKNIGHT' and showTank.monitorBloodDK) or (class == 'MONK' and showTank.monitorBrewMonk) then
+		return true
+	end
+
+	return
+end
+
+function module:UpdateAllFrames(group)
+	if InCombatLockdown() then
+		module.NeedsUpdating = true
+		module:RegisterEvent('PLAYER_REGEN_ENABLED')
+		return	
+	end
+
+	if group and group ~= 'All' then
+		UF:CreateAndUpdateHeaderGroup(group)
+	else
+		UF:CreateAndUpdateHeaderGroup('party')
+
+		for i = 1, 3 do
+			UF:CreateAndUpdateHeaderGroup('raid'..i)
+		end	
+	end
+end
+
+function module:PLAYER_REGEN_ENABLED()
+	if module.NeedsUpdating then
+		module:UpdateAllFrames()
+		module.NeedsUpdating = nil	
+	end
+
+	module:UnregisterEvent('PLAYER_REGEN_ENABLED')
+end
+
+function module:Update_Frames(frame, db)
+	if not (frame or frame.db or frame:IsShown() or E.db.hop.enable or (frame.unitframeType and E.db.hop.unitframe[frame.unitframeType].healerOnly)) then return end
+
+	if not db.enable or db.power.enable then return end
+
+	if frame.isForced then
+		if random(1, 3) ~= 1 then
+			return
+		end
+	elseif not shouldDisplayBar(frame) then
+		return
+	end
+
+	frame.USE_POWERBAR = true
+	frame.POWERBAR_DETACHED = false
+	frame.USE_INSET_POWERBAR = not frame.POWERBAR_DETACHED and db.power.width == 'inset' and frame.USE_POWERBAR
+	frame.USE_MINI_POWERBAR = (not frame.POWERBAR_DETACHED and db.power.width == 'spaced' and frame.USE_POWERBAR)
+	frame.USE_POWERBAR_OFFSET = (db.power.width == 'offset' and db.power.offset ~= 0) and frame.USE_POWERBAR and not frame.POWERBAR_DETACHED
+	frame.POWERBAR_OFFSET = frame.USE_POWERBAR_OFFSET and db.power.offset or 0
+	frame.POWERBAR_HEIGHT = not frame.USE_POWERBAR and 0 or db.power.height
+	frame.POWERBAR_WIDTH = frame.USE_MINI_POWERBAR and (frame.UNIT_WIDTH - (UF.BORDER*2))*0.5 or (frame.POWERBAR_DETACHED and db.power.detachedWidth or (frame.UNIT_WIDTH - ((UF.BORDER+UF.SPACING)*2)))
+	frame.CLASSBAR_WIDTH = frame.UNIT_WIDTH - frame.PORTRAIT_WIDTH - (frame.ORIENTATION == 'MIDDLE' and (frame.POWERBAR_OFFSET*2) or frame.POWERBAR_OFFSET)
+	frame.CLASSBAR_YOFFSET = (not frame.USE_CLASSBAR or not frame.CLASSBAR_SHOWN or frame.CLASSBAR_DETACHED) and 0 or (frame.USE_MINI_CLASSBAR and (UF.SPACING+(frame.CLASSBAR_HEIGHT*0.5)) or (frame.CLASSBAR_HEIGHT - (UF.BORDER-UF.SPACING)))
+	frame.USE_INFO_PANEL = not frame.USE_MINI_POWERBAR and not frame.USE_POWERBAR_OFFSET and db.infoPanel.enable
+	frame.BOTTOM_OFFSET = UF:GetHealthBottomOffset(frame)
+
+	UF:Configure_InfoPanel(frame)
+	UF:Configure_HealthBar(frame)
+	UF:Configure_Power(frame)
+
+	frame:UpdateAllElements('ElvUI_UpdateAllElements')
+end
+
+function module:UpdateOptions(group)
+	if not (UF.Initialized or E.private.unitframe.enable) then return end
+	module.db = E.db.hop
+
+	if module.db.enable then
+		if not module:IsHooked(UF, 'Update_RaidFrames') then
+			-- print('|cff00FF98HOP:|r Enabled')
+			module:SecureHook(UF, 'Update_PartyFrames', module.Update_Frames)
+			module:SecureHook(UF, 'Update_RaidFrames', module.Update_Frames)
+			module:RegisterEvent('PLAYER_ROLES_ASSIGNED', module.UpdateAllFrames)
+		end
+	elseif not module.db.enable then
+		if module:IsHooked(UF, 'Update_RaidFrames') then
+			-- print('|cff00FF98HOP:|r Disabled')
+			module:Unhook(UF, 'Update_RaidFrames')
+			module:UnregisterEvent('PLAYER_ROLES_ASSIGNED')
+		end
+	end
+end
+
+function module:Initialize()
+	EP:RegisterPlugin(AddOnName, GetOptions)
+	if not UF.Initialized then return end
+	
+	module:UpdateOptions()
+	hooksecurefunc(E, 'UpdateAll', module.UpdateOptions)
+end
+
+E.Libs.EP:HookInitialize(module, module.Initialize)
---
+++ README.md
@@ -0,0 +1,28 @@
+[![Discord](https://img.shields.io/discord/1162274244487561216?style=for-the-badge&logo=discord&label=Discord)](https://repoocreforged.dev/discord) [![GitHub Repo stars](https://img.shields.io/github/stars/repooc/ElvUI_HealerOnlyPower?style=for-the-badge&logo=github)](https://github.com/Repooc/ElvUI_HealerOnlyPower)
+
+# Healer Only Power (ElvUI Plugin)
+[![Version (CF)](https://img.shields.io/curseforge/v/938985?style=for-the-badge&logo=curseforge)](https://curseforge.com/wow/addons/elvui-healer-only-power/) [![GitHub tag (with filter)](https://img.shields.io/github/v/tag/Repooc/ElvUI_HealerOnlyPower?logo=github&label=Version&style=for-the-badge)](https://github.com/Repooc/ElvUI_HealerOnlyPower/tags)
+
+
+[![Patreon](https://img.shields.io/badge/Patreon-F96854?style=for-the-badge&logo=patreon&logoColor=white)](https://www.patreon.com/repoocreforged) [![ko-fi](https://img.shields.io/badge/_-KO--FI-red?style=for-the-badge&logo=ko-fi&logoColor=white)](https://ko-fi.com/repoocreforged) 
+
+
+Have you ever wanted to streamline your ElvUI unitframes by hiding power bars for non-healer roles?
+
+**Healer Only Power** is the solution! This ElvUI plugin allows you to focus on what matters by displaying power bars exclusively for the healers in your party and raid 1-3 unitframes.
+
+### Features:
+- **Simplified Interface:** Enhance your healing experience by decluttering unitframes.
+- **Role-Specific Visibility:** Power bars appear only for characters in ***healer roles*\***.
+- **Tank Specific Override:** When the healer only option is enabled, you can also override the feature to monitor a couple specific tank's power.
+
+**\*** *This was and has been the idea of the plugin with the one expection for those healers that like to monitor a couple specific classes.*
+
+### Future Updates:
+Anticipate an evolving experience! I strive to keep Healer Only Power up to everyone's needs. While I welcome and value your feedback, please understand that not all suggestions may be implemented. I'll carefully consider each idea for additional customization options and features, ensuring that any future updates align with the essence of this plugin or at the very minimum, not break anything lol.
+
+Your input is crucial in shaping the journey of this addon. Thank you for being part of the community driving its growth!
+
+*Note: Ensure you have ElvUI installed for optimal functionality.*
+
+Download now and elevate your healing gameplay!
